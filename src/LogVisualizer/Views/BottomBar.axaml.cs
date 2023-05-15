fdd836688c6e8bbe62562415e85a2c01b72177b4
Jim.Jiang <375324644@qq.com>
2023-05-15 23:10:36 +0800

    misc: add bottom bar

---
+++ src/LogVisualizer/Views/BottomBar.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LogVisualizer.Views
+{
+    public partial class BottomBar : UserControl
+    {
+        public BottomBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
