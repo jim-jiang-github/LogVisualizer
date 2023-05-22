e0874e1bb14065882c65b0a31aecc7de7c8b6164
Jim.Jiang <375324644@qq.com>
2023-05-22 20:32:56 +0800

    misc: add UT

---
+++ src/LogVisualizer.Test/Startup.cs
@@ -0,0 +1,27 @@
+﻿using LogVisualizer.Services;
+using LogVisualizer.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogVisualizer.Test
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<UpgradeService>();
+            services.AddSingleton<UpgraderViewModel>();
+            services.AddSingleton<GitService>();
+            services.AddScoped<MenuBarViewModel>();
+            services.AddScoped<MainWindowViewModel>();
+            services.AddScoped<SideBarViewModel>();
+            services.AddScoped<BottomBarViewModel>();
+            services.AddSingleton<SchemaConfigViewModel>();
+        }
+    }
+}
--- src/LogVisualizer/ViewModels/SchemaConfigViewModel.cs
+++ src/LogVisualizer/ViewModels/SchemaConfigViewModel.cs
@@ -11,71 +11,127 @@ using LogVisualizer.I18N;
 using CommunityToolkit.Mvvm.Input;
 using System.Threading;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace LogVisualizer.ViewModels
 {
     public partial class SchemaConfigViewModel : ViewModelBase
     {
+        public partial class SchemaCreate : ViewModelValidator
+        {
+            public class ValidationAttributeImpl : ValidationAttribute 
+            {
+                protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+                {
+                    return base.IsValid(value, validationContext);
+                }
+            }
+            private SchemaConfigViewModel _owner;
+
+            [RegularExpression(@"^[a-zA-Z0-9_\-\.]+$")]
+            [NotifyCanExecuteChangedFor(nameof(CreateSchemaCommand))]
+            [ValidationAttributeImpl]
+            [Required(ErrorMessageResourceType = typeof(I18NResource), ErrorMessageResourceName = I18NResource.Schema_Creator_Schema_Name_Valid)]
+            [ObservableProperty]
+            private string? _schemaName = string.Empty;
+
+            [NotifyCanExecuteChangedFor(nameof(CreateSchemaCommand))]
+            [Required(ErrorMessageResourceType = typeof(I18NResource), ErrorMessageResourceName = I18NResource.Schema_Creator_Schema_Repo_Valid)]
+            [ObservableProperty]
+            private string? _schemaRepo = string.Empty;
+
+            [NotifyCanExecuteChangedFor(nameof(CreateSchemaCommand))]
+            [Required(ErrorMessageResourceType = typeof(I18NResource), ErrorMessageResourceName = I18NResource.Schema_Creator_Schema_Branch_Valid)]
+            [ObservableProperty]
+            private string? _schemaBranch = string.Empty;
+
+            public SchemaCreate(SchemaConfigViewModel owner)
+            {
+                _owner = owner;
+            }
+
+            [RelayCommand(CanExecute = nameof(CanCreateSchema))]
+            private async Task CreateSchema(object schemaNameAndRepo)
+            {
+                if (!HasErrors)
+                {
+                    _owner.IsUpdating = true;
+                    await Task.Delay(5555);
+                    _owner.IsUpdating = false;
+                }
+                //ValidateAllProperties();
+                //if (HasErrors)
+                //{
+                //    return;
+                //}
+                //int a = 1;
+                //return;
+                //try
+                //{
+                //    if (schemaNameAndRepo is not ReadOnlyCollection<object> x)
+                //    {
+                //        return;
+                //    }
+                //    string schemaName = (string)x[0];
+                //    string schemaRepo = (string)x[1];
+                //    string schemaBranch = (string)x[2];
+                //    var result = await _gitService.CloneTo(schemaRepo, schemaBranch, schemaName);
+                //}
+                //catch (OperationCanceledException)
+                //{
+                //}
+            }
+            private bool CanCreateSchema(object schemaNameAndRepo)
+            {
+                ValidateAllProperties();
+                return !HasErrors;
+                return false;
+                if (schemaNameAndRepo is not ReadOnlyCollection<object> x)
+                {
+                    return false;
+                }
+                if (x.Count != 3)
+                {
+                    return false;
+                }
+                if (x[0] is not string schemaName || x[1] is not string schemaRepo || x[2] is not string schemaBranch)
+                {
+                    return false;
+                }
+                if (string.IsNullOrEmpty(schemaName) || string.IsNullOrEmpty(schemaRepo) || string.IsNullOrEmpty(schemaBranch))
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
         private GitService _gitService;
 
+        [ObservableProperty]
+        private bool _isUpdating = false;
         [ObservableProperty]
         private string? _currentName;
         [ObservableProperty]
         private bool _needsAttention = false;
         [ObservableProperty]
         private ObservableCollection<string> _branches;
+        [ObservableProperty]
+        private SchemaCreate _create;
 
         public SchemaConfigViewModel(GitService gitService)
         {
+            Create = new SchemaCreate(this);
             _gitService = gitService;
             _branches = new ObservableCollection<string>();
             NeedsAttention = true;
             I18NKeys.Schema_No_Source.BindingExpression(this, x => x.CurrentName);
 
-            //_ = Task.Run(async () =>
-            //{
-            //    var branches = await gitService.GetAllOriginBranches(Configuration.Instance.Schema?.SchemaRepo).WithLoadingMask();
-            //    Branches = new ObservableCollection<string>(branches);
-            //});
-        }
-
-        [RelayCommand(CanExecute = nameof(CanCreateSchema))]
-        private async Task CreateSchema(object schemaNameAndRepo)
-        {
-            try
+            IsUpdating = true;
+            _ = Task.Run(async () =>
             {
-                if (schemaNameAndRepo is not ReadOnlyCollection<object> x)
-                {
-                    return;
-                }
-                string schemaName = (string)x[0];
-                string schemaRepo = (string)x[1];
-                string schemaBranch = (string)x[2];
-                var result = await _gitService.CloneTo(schemaRepo, schemaBranch, schemaName);
-            }
-            catch (OperationCanceledException)
-            {
-            }
-        }
-        private bool CanCreateSchema(object schemaNameAndRepo)
-        {
-            if (schemaNameAndRepo is not ReadOnlyCollection<object> x)
-            {
-                return false;
-            }
-            if (x.Count != 3)
-            {
-                return false;
-            }
-            if (x[0] is not string schemaName || x[1] is not string schemaRepo || x[2] is not string schemaBranch)
-            {
-                return false;
-            }
-            if (string.IsNullOrEmpty(schemaName) || string.IsNullOrEmpty(schemaRepo) || string.IsNullOrEmpty(schemaBranch))
-            {
-                return false;
-            }
-            return true;
+                IsUpdating = false;
+            });
         }
     }
 }
