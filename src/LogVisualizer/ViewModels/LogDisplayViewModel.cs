b12a3ca20ffc73231e09343ad1381771c19423f1
Jim.Jiang <375324644@qq.com>
2023-06-05 15:41:25 +0800

    misc: mock data for test.

--- src/LogVisualizer/ViewModels/LogDisplayViewModel.cs
+++ src/LogVisualizer/ViewModels/LogDisplayViewModel.cs
@@ -15,11 +15,84 @@ namespace LogVisualizer.ViewModels
 {
     public class LogDisplayViewModel : ViewModelBase
     {
+        public struct Item
+        {
+            public int Index { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public class ItemCollection : IList
+        {
+            public object? this[int index]
+            {
+                get
+                {
+                    return new Item()
+                    {
+                        Index = index,
+                        Name = $"{index}xxx",
+                        Age = index
+                    };
+                }
+                set => throw new NotImplementedException();
+            }
+
+            public bool IsFixedSize => true;
+
+            public bool IsReadOnly => true;
+
+            public int Count => int.MaxValue;
+
+            public bool IsSynchronized => true;
+
+            public object SyncRoot => this;
+
+            public int Add(object? value)
+            {
+                return -1;
+            }
+
+            public void Clear()
+            {
+            }
+
+            public bool Contains(object? value)
+            {
+                return false;
+            }
+
+            public void CopyTo(Array array, int index)
+            {
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                yield return -1;
+            }
+
+            public int IndexOf(object? value)
+            {
+                return -1;
+            }
+
+            public void Insert(int index, object? value)
+            {
+            }
+
+            public void Remove(object? value)
+            {
+            }
+
+            public void RemoveAt(int index)
+            {
+            }
+        }
         public IList? Items { get; }
 
         public LogDisplayViewModel()
         {
-            //Items = scenario.LogSource?.GetRows();
+            Items = new ItemCollection();
         }
     }
 
